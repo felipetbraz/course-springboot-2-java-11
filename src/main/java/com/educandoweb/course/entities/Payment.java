75edef1543f281e66c80fb95bbd76d788c149df1
Felipe Braz <felipe.tbraz@gmail.com>
2019-10-01 22:02:43 -0300

    Refactor: no more Jackson in entities

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -11,8 +11,6 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
@@ -22,8 +20,7 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	
-	@JsonIgnore
+
 	@OneToOne
 	@MapsId
 	private Order order;
