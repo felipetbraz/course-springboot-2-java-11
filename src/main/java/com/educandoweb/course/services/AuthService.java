33f8037854a45f4795e53bb630553b832ce36592
Felipe Braz <felipe.tbraz@gmail.com>
2019-10-18 01:15:23 -0300

    Forgot password, Logger

--- src/main/java/com/educandoweb/course/services/AuthService.java
+++ src/main/java/com/educandoweb/course/services/AuthService.java
@@ -1,12 +1,18 @@
 package com.educandoweb.course.services;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.course.dto.CredentialsDTO;
 import com.educandoweb.course.dto.TokenDTO;
@@ -15,16 +21,22 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.security.JWTUtil;
 import com.educandoweb.course.services.exceptions.JWTAuthenticationException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 
 public class AuthService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AuthService.class);
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
 	@Autowired
 	private JWTUtil jwtUtil;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -67,5 +79,40 @@ public class AuthService {
 	public TokenDTO refreshToken() {
 		User user = authenticated();
 		return new TokenDTO(user.getEmail(), jwtUtil.generateToken(user.getEmail()));
+	}
+	
+	@Transactional
+	public void sendNewPassword(String email) {
+		User user = userRepository.findByEmail(email);
+		if(user == null) {
+			throw new ResourceNotFoundException("Email not found");
+		}
+		String newPass = newPassword();
+		user.setPassword(passwordEncoder.encode(newPass));
+		
+		userRepository.save(user);
+		LOG.info("New password: " + newPass);		
+	}
+	
+	private String newPassword() {
+		char[] vect = new char[10];
+		for(int i=0; i<10; i++) {
+			vect[i] = randomChar();
+		}
+		return new String(vect);
+	}
+	
+	private char randomChar() {
+		Random rand = new Random();
+		int opt = rand.nextInt();
+		if(opt == 0) {//generate digit
+			return (char) (rand.nextInt(10) + 48);
+		}
+		else if(opt == 1){//generate uppercase letter
+			return (char) (rand.nextInt(26) + 65);
+		}
+		else {//generate lowercase letter
+			return (char) (rand.nextInt(26) + 97);
+		}
 	}
 }
