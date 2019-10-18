15449a4097584b041d0a11650270be452f43b0eb
Felipe Braz <felipe.tbraz@gmail.com>
2019-10-18 19:42:31 -0300

    Update Order

--- src/main/java/com/educandoweb/course/resources/OrderResource.java
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,7 @@ public class OrderResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	
 	@GetMapping(value = "/myorders")
 	public ResponseEntity<List<OrderDTO>> findByClient(){
 		List<OrderDTO> list = service.findByClient();
@@ -63,5 +65,12 @@ public class OrderResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(orderDTO.getId()).toUri();
 		return ResponseEntity.created(uri).body(orderDTO);
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<OrderDTO> update(@PathVariable Long id, @RequestBody OrderDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 }
