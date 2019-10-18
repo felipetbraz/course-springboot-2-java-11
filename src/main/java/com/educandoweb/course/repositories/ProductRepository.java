9d0d63b2fea9b2dc3ce5be3e50647a0b4db53247
Felipe Braz <felipe.tbraz@gmail.com>
2019-10-18 18:04:29 -0300

    Managing product categories

--- src/main/java/com/educandoweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.course.entities.Category;
@@ -13,5 +14,5 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 	
 	@Transactional(readOnly = true)
 	@Query("SELECT obj FROM Product obj INNER JOIN obj.categories cats WHERE :category IN cats")
-	Page<Product> findByCategory(Category category, Pageable pageable);
+	Page<Product> findByCategory(@Param("category") Category category, Pageable pageable);
 }
